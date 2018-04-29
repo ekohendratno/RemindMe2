80552589d9c610b22f6a5ad84e7cad9895a0acac
Server-Cadangan <eko.hendratno@gmail.com>
2018-04-29 22:38:22 +0700

    Initial commit

---
+++ app/src/main/java/id/kopas/berkarya/remindme2/AlarmReceiver.java
@@ -0,0 +1,53 @@
+package id.kopas.berkarya.remindme2;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Tatson on 05-Apr-17.
+ */
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    Ringtone ringtone;
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        //this will update the UI with message
+        MainActivity inst = MainActivity.instance();
+        inst.setAlarmText("Wake up! Wake up!");
+
+        //this will sound the alarm tone
+        //this will sound the alarm once, if you wish to
+        //raise alarm in loop continuously then use MediaPlayer and setLooping(true)
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        //After 1s stop the alarm
+        // You can adjust the time depending upon your requirement.
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ringtone.stop();
+            }
+        }, 1000);
+
+        //this will send a notification message
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
